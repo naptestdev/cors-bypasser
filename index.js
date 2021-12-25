84a04f983ad590f8455a48f2137556be472cd4e0
napthedev <phongna.dev@gmail.com>
2021-12-25 07:25:19 +0700

    update

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ app.use("*", (req, res) => {
     res.send("Cors Bypasser. Just go to /:url to use the proxy");
   } else {
     try {
-      request
-        .get(req.originalUrl.slice(1), {
-          headers: req.query.origin
-            ? {
-                origin: req.query.origin,
-                referer: req.query.origin,
-                referrer: req.query.origin,
-              }
-            : {},
-        })
-        .pipe(res);
+      request(req.originalUrl.slice(1), {
+        method: req.method,
+        headers: req.query.origin
+          ? {
+              origin: req.query.origin,
+              referer: req.query.origin,
+              referrer: req.query.origin,
+            }
+          : {},
+        body: req.body,
+      }).pipe(res);
     } catch (error) {
       if (!res.headersSent) res.sendStatus(500);
     }
